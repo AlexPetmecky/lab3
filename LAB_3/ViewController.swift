c22cbd6090cdaf823e03f6576aeadb7a7ea0373a
alexpetmecky <petmeckyalex@gmail.com>
2024-10-15 13:05:45 -0500

    Initial Commit

---
+++ LAB_3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LAB_3
+//
+//  Created by Alex Petmecky on 10/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LAB_3Tests/LAB_3Tests.swift
@@ -0,0 +1,17 @@
+//
+//  LAB_3Tests.swift
+//  LAB_3Tests
+//
+//  Created by Alex Petmecky on 10/15/24.
+//
+
+import Testing
+@testable import LAB_3
+
+struct LAB_3Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
